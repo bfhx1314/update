55f775a9fcb07e62610065549052a159000a9b4e
bfhx1314 <bfhx1314@163.com>
2015-08-09 11:06:02 +0800

    修改

--- update/src/com/limn/update/config/StaticHttp.java
+++ update/src/com/limn/update/config/StaticHttp.java
@@ -2,15 +2,17 @@ package com.limn.update.config;
 
 public class StaticHttp {
 	
-	
-	public static String REQUEST_IP = "http://172.16.24.95:8080/update/";
-	
-	public static String DOWNLOAD_IP = "http://172.16.24.95:8080/update/apk/";
+	//请求地址
+	public static String REQUEST_IP = "http://172.16.24.95:80";
 	
 	
-	public static String INDEX = REQUEST_IP + "index.do";
 	
-	public static String GETVERSION = REQUEST_IP + "getVersion.do";
+	
+	public static String DOWNLOAD_IP = REQUEST_IP + "/apk/";
+	
+	public static String INDEX = REQUEST_IP + "/index.do";
+	
+	public static String GETVERSION = REQUEST_IP + "/getVersion.do";
 	
 	public static String APK_FILES_PATH = "sdcard/apkDownLoad";
 
--- update/src/com/limn/update/listener/ClickFileListener.java
+++ update/src/com/limn/update/listener/ClickFileListener.java
@@ -5,6 +5,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
@@ -19,7 +20,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Handler;
 import android.os.Message;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -27,26 +27,28 @@ import android.widget.Toast;
 
 public class ClickFileListener implements OnClickListener {
 	private String fileDownLoadPath = null;
-	
+
 	// 是否在进行下载
 	private boolean isLoading = false;
 
 	private Context act = null;
+
 	/*
 	 * 一个构造方法, 将Button对象做为参数
 	 */
-	public ClickFileListener(String URL,Context mContext) {
+	public ClickFileListener(String URL, Context mContext) {
 		this.act = mContext;
 		fileDownLoadPath = URL;
 	}
 
+	@SuppressLint("ShowToast")
 	public void onClick(View v) {
 		String name = (String) ((Button) v).getText();
 		if (name.equals("安装")) {
-			
-			
-			String filePath = StaticHttp.APK_FILES_PATH + "/" + fileDownLoadPath;
-			
+
+			String filePath = StaticHttp.APK_FILES_PATH + "/"
+					+ fileDownLoadPath;
+
 			File file = new File(filePath);
 
 			if (!file.exists()) {
@@ -56,15 +58,15 @@ public class ClickFileListener implements OnClickListener {
 			openFile(filePath);
 			return;
 		}
-		
+
 		// 下载的进程
 		if (!isLoading && name.equals("下载")) {
 			isLoading = true;
-			new Thread(new DownLoadFile(fileDownLoadPath, ((Button) v))).start();
+			new Thread(new DownLoadFile(fileDownLoadPath, ((Button) v)))
+					.start();
 
 		}
 	}
-	
 
 	/**
 	 * 打开APK
@@ -74,7 +76,8 @@ public class ClickFileListener implements OnClickListener {
 	private void openFile(String fileDownLoadPath) {
 		Intent intentInstall = new Intent();
 		intentInstall.setAction(android.content.Intent.ACTION_VIEW);
-		intentInstall.setDataAndType(Uri.fromFile(new File(fileDownLoadPath)), "application/vnd.android.package-archive");
+		intentInstall.setDataAndType(Uri.fromFile(new File(fileDownLoadPath)),
+				"application/vnd.android.package-archive");
 		intentInstall.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 		act.startActivity(intentInstall);
 	}
@@ -87,7 +90,6 @@ public class ClickFileListener implements OnClickListener {
 	 */
 	class DownLoadFile implements Runnable {
 
-		private String filePath = null;
 		private String fileDownLoadPath = null;
 		private Button button = null;
 		private int fileSize = 0;
@@ -101,19 +103,7 @@ public class ClickFileListener implements OnClickListener {
 		@Override
 		public void run() {
 
-			try {
-				down_file(fileDownLoadPath);
-			} catch (IOException e) {
-				
-				StaticHttp.APK_FILES_PATH = StaticHttp.APK_FILES_PATH + "1";
-				
-				try {
-					down_file(fileDownLoadPath);
-				} catch (IOException e1) {
-					
-				}
-				
-			}
+			down_file(fileDownLoadPath,false);
 
 		}
 
@@ -124,60 +114,81 @@ public class ClickFileListener implements OnClickListener {
 		 * @param path
 		 * @throws IOException
 		 */
-		public void down_file(String path) throws IOException {
+		public void down_file(String path, boolean isAgain) {
 
 			String url = path;
-			
-			url = StaticHttp.DOWNLOAD_IP + encode(url, "UTF-8");
-
-			// String fileNameTextView.setText(fileName);
-			// 获取文件名
-			URL myURL = new URL(url);
-
-			URLConnection conn = myURL.openConnection();
-			conn.connect();
-
-			InputStream is = conn.getInputStream();
-
-			fileSize = conn.getContentLength();// 根据响应获取文件大小
-			if (fileSize <= 0)
-				throw new RuntimeException("无法获知文件大小 ");
-			if (is == null)
-				throw new RuntimeException("stream is null");
-
-			path = StaticHttp.APK_FILES_PATH + "/" + path;
-			
-			File file = new File(path);
-			if (!file.getParentFile().exists()) {
-				file.getParentFile().mkdirs();
-			}
-			if(file.exists()){
-				file.delete();
-			}
-
-			 FileOutputStream fos = new FileOutputStream(file.getAbsolutePath(),true);
-
-			// 把数据存入路径+文件名
-			byte buf[] = new byte[1024];
-			downLoadFileSize = 0;
-			sendMsg(0);
-			do {
-				// 循环读取
-				int numread = is.read(buf);
-				if (numread == -1) {
-					break;
-				}
-				fos.write(buf, 0, numread);
-				downLoadFileSize += numread;
-				sendMsg(1);// 更新进度条
-			} while (true);
-			sendMsg(2);// 通知下载完成
+			URL myURL = null;
 			try {
+				url = StaticHttp.DOWNLOAD_IP + encode(url, "UTF-8");
+				myURL = new URL(url);
+			} catch (UnsupportedEncodingException e2) {
+				e2.printStackTrace();
+			} catch (MalformedURLException e1) {
+				e1.printStackTrace();
+			}
+
+			URLConnection conn = null;
+			InputStream is = null;
+			FileOutputStream fos = null;
+			try {
+				conn = myURL.openConnection();
+				conn.connect();
+
+				is = conn.getInputStream();
+
+				fileSize = conn.getContentLength();// 根据响应获取文件大小
+				if (fileSize <= 0)
+					throw new RuntimeException("无法获知文件大小 ");
+				if (is == null)
+					throw new RuntimeException("stream is null");
+
+				path = StaticHttp.APK_FILES_PATH + "/" + path;
+
+				File file = new File(path);
+				if (!file.getParentFile().exists()) {
+					file.getParentFile().mkdirs();
+				}
+				if (file.exists()) {
+					file.delete();
+				}
+
+				fos = new FileOutputStream(file.getAbsolutePath(), true);
+				// 把数据存入路径+文件名
+				byte buf[] = new byte[1024];
+				downLoadFileSize = 0;
+				sendMsg(0);
+				do {
+					// 循环读取
+					int numread = is.read(buf);
+					if (numread == -1) {
+						break;
+					}
+					fos.write(buf, 0, numread);
+					downLoadFileSize += numread;
+					sendMsg(1);// 更新进度条
+				} while (true);
+				sendMsg(2);// 通知下载完成
+				conn = null;
 				fos.flush();
 				fos.close();
 				is.close();
-			} catch (Exception ex) {
-				Log.e("tag", "error: " + ex.getMessage(), ex);
+			} catch (IOException ex) {
+				
+				StaticHttp.APK_FILES_PATH = StaticHttp.APK_FILES_PATH + "1";
+				if(!isAgain){
+					down_file(fileDownLoadPath,true);
+				}
+				
+			} finally {
+				conn = null;
+				try {
+					fos.flush();
+					fos.close();
+					is.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
 			}
 		}
 
@@ -186,7 +197,6 @@ public class ClickFileListener implements OnClickListener {
 			msg.what = flag;
 			handler.sendMessage(msg);
 		}
-
 
 		/**
 		 * 替换字符串卷
@@ -199,8 +209,9 @@ public class ClickFileListener implements OnClickListener {
 		 * @throws UnsupportedEncodingException
 		 *             不支持的字符集
 		 */
-		public String encode(String str , String charset) throws UnsupportedEncodingException {
-			
+		public String encode(String str, String charset)
+				throws UnsupportedEncodingException {
+
 			String zhPattern = "[\\u4e00-\\u9fa5]";
 			Pattern p = Pattern.compile(zhPattern);
 			Matcher m = p.matcher(str);
@@ -212,7 +223,8 @@ public class ClickFileListener implements OnClickListener {
 			return b.toString().replace(" ", "%20");
 		}
 
-		@SuppressLint("HandlerLeak") private Handler handler = new Handler() {
+		@SuppressLint("HandlerLeak")
+		private Handler handler = new Handler() {
 			@Override
 			public void handleMessage(Message msg) {// 定义一个Handler，用于处理下载线程与UI间通讯
 				if (!Thread.currentThread().isInterrupted()) {
